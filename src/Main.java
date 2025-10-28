6f9ef08776bcbf78e863190f41d09e2bb25fbc71
simon molander <simon.molander@yh.nackademin.se>
2025-10-28 13:39:22 +0100

    test1

--- src/Main.java
+++ src/Main.java
@@ -6,6 +6,6 @@ public class Main {
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("testar med detta simon muhahahsdds");
 
-        System.out.println("simon skrev det här hehehehehe ");
+        System.out.println("simon skrev det här hehehehehejjj ");
     }
 }

0ca53524baab9ff2294efc9712cc314711fd4baa
simon molander <simon.molander@yh.nackademin.se>
2025-10-28 13:37:53 +0100

    test

--- src/Main.java
+++ src/Main.java
@@ -5,5 +5,7 @@ public class Main {
         //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
         // to see how IntelliJ IDEA suggests fixing it.
         System.out.println("testar med detta simon muhahahsdds");
+
+        System.out.println("simon skrev det här hehehehehe ");
     }
 }
